dc218bdf0cd729f3e40fa5b0946a048a38c1a480
YUN <yunmyeonghun@gmail.com>
2021-12-04 12:07:47 +0900

    study 02

--- main.py
+++ main.py
@@ -1,8 +1,42 @@
+from typing import List
+from uuid import UUID, uuid4
 from fastapi import FastAPI
 
+from models import Gender, Role, User
+
 app = FastAPI()
+
+db: List[User] = [
+    User(
+        # Fixed UUID
+        id=UUID("af7bcda7-d772-4d4d-a9e6-0b24fe1b2137"),
+        first_name="Jamila",
+        last_name="Ahmed",
+        gender=Gender.female,
+        roles=[Role.student]
+    ),
+    User(
+        # Auto generated on app start
+        id=uuid4(),
+        first_name="Ales",
+        last_name="Jones",
+        gender=Gender.male,
+        roles=[Role.admin, Role.user]
+    )
+]
 
 
 @app.get("/")
 async def root():
     return {"Hello": "Mundo"}
+
+
+@app.get("/api/v1/users")
+async def fetch_users():
+    return db
+
+
+@app.post("/api/v1/users")
+async def register_user(user: User):
+    db.append(user)
+    return {"id": user.id}
