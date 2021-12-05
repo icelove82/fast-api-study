417b31791d6b30de6a5534153e29572b90044f8e
YUN <yunmyeonghun@gmail.com>
2021-12-05 16:45:37 +0900

    study 04

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import List
 from uuid import UUID, uuid4
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from models import Gender, Role, User
 
@@ -48,3 +48,7 @@ async def delete_user(user_id: UUID):
         if user.id == user_id:
             db.remove(user)
             return
+    raise HTTPException(
+        status_code=404,
+        detail=f"user with id: {user_id} does not exists"
+    )
