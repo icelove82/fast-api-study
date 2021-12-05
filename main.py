5248f0b62418ac418674abcad36505933b70b472
YUN <yunmyeonghun@gmail.com>
2021-12-05 17:05:15 +0900

    study 05

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from typing import List
 from uuid import UUID, uuid4
 from fastapi import FastAPI, HTTPException
 
-from models import Gender, Role, User
+from models import Gender, Role, User, UserUpdateRequest
 
 app = FastAPI()
 
@@ -47,6 +47,25 @@ async def delete_user(user_id: UUID):
     for user in db:
         if user.id == user_id:
             db.remove(user)
+            return
+    raise HTTPException(
+        status_code=404,
+        detail=f"user with id: {user_id} does not exists"
+    )
+
+
+@app.put("/api/v1/users/{user_id}")
+async def update_user(user_update: UserUpdateRequest, user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            if user_update.first_name is not None:
+                user.first_name = user_update.first_name
+            if user_update.last_name is not None:
+                user.last_name = user_update.last_name
+            if user_update.middle_name is not None:
+                user.middle_name = user_update.middle_name
+            if user_update.roles is not None:
+                user.roles = user_update.roles
             return
     raise HTTPException(
         status_code=404,
